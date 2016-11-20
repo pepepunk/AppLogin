38323ca9ba73557e96f10be60c4571b60f63d442
pepepunk <jmppk95@hotmail.com>
2016-11-19 22:40:34 -0600

    v2

---
+++ app/src/main/java/com/pepepunk/app/applogin/eliminarActivity.java
@@ -0,0 +1,13 @@
+package com.pepepunk.app.applogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class eliminarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar);
+    }
+}
